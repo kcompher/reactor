0c2142c3616d1ff7f3b51a73e38e3d2947629156
Kevin <kevin@ospatial.com>
2018-12-04 16:04:04 -0500

    init notes

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import Coins from './Coins/Coins';
+import Notes from './Notes/Notes';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
+import { notes1, notes2 } from './Notes/data';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    // The first time we load the notes1
+    this.state = {
+      notes: notes1
+    };
+  }
+
+  componentDidMount() {
+    // After 10 seconds we concatenate our data with notes2
+    setTimeout(() => {
+      this.setState({
+        notes: [...this.state.notes, ...notes2]
+      });
+    }, 10000);
+  }
+
   render() {
     return (
       <div className="App">
-        <Header title="Crypto Coins Exchanger" />
+        <Header title="Notes" />
 
         <Content>
-          <Coins />
+          <Notes notes={this.state.notes} />
         </Content>
 
         <Footer />
--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const unmountButton = document.getElementById('unmount');
+
+function unmount() {
+  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+  document.getElementById('unmountMessage').style.display = 'block';
+  unmountButton.remove();
+}
+
+unmountButton.addEventListener('click', unmount);
+
+document.getElementById('unmountMessage').style.display = 'none';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
