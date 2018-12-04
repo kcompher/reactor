ee51516bde8019fef911e6e6aabfe192b4a421ee
Kevin <kevin@ospatial.com>
2018-12-04 15:56:43 -0500

    crpto example

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Todo from './Todo/Todo';
+import Coins from './Coins/Coins';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
@@ -9,10 +9,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header title="Todo List" />
+        <Header title="Crypto Coins Exchanger" />
 
         <Content>
-          <Todo />
+          <Coins />
         </Content>
 
         <Footer />
