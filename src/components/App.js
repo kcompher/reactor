0ff00b97bf936b40e70cc00abdb9ade010c621de
Kevin <kevin@ospatial.com>
2018-12-05 11:07:43 -0500

    chart

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,50 @@
 import React, { Component } from 'react';
-import Notes from './Notes/Notes';
+import Chart from './Chart/Chart';
 import Header from '../shared/components/layout/Header';
 import Content from '../shared/components/layout/Content';
 import Footer from '../shared/components/layout/Footer';
-import { notes1, notes2 } from './Notes/data';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    // The first time we load the notes1
     this.state = {
-      notes: notes1
+      chartType: 'line'
     };
+
+    this.columns = [
+      ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
+      ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
+      ['XRP', 100, 200, 300, 500, 400, 300],
+    ];
   }
 
-  componentDidMount() {
-    // After 10 seconds we concatenate our data with notes2
-    setTimeout(() => {
-      this.setState({
-        notes: [...this.state.notes, ...notes2]
-      });
-    }, 10000);
+  setBarChart = () => {
+    this.setState({
+      chartType: 'bar'
+    });
+  }
+
+  setLineChart = () => {
+    this.setState({
+      chartType: 'line'
+    });
   }
 
   render() {
-    return (
+		return (
       <div className="App">
-        <Header title="Notes" />
+        <Header title="Charts" />
 
         <Content>
-          <Notes notes={this.state.notes} />
+          <Chart columns={this.columns} chartType={this.state.chartType} />
+
+          <p>
+            Chart Type
+            <button onClick={this.setBarChart}>bar</button>
+            <button onClick={this.setLineChart}>Line</button>
+          </p>
         </Content>
 
         <Footer />
--- src/index.js
+++ src/index.js
@@ -4,17 +4,5 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const unmountButton = document.getElementById('unmount');
-
-function unmount() {
-  ReactDOM.unmountComponentAtNode(document.getElementById('root'));
-  document.getElementById('unmountMessage').style.display = 'block';
-  unmountButton.remove();
-}
-
-unmountButton.addEventListener('click', unmount);
-
-document.getElementById('unmountMessage').style.display = 'none';
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
